353d03438c55153780e150d93ecbb98a3ebae976
billClinton048 <clintonkaranja46@gmail.com>
2024-08-24 01:07:35 +0300

    message

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      bgcolor: '#060278',
-      textred: '#cf6a0c',
-      bghover: '#c7d9f0',
-      bgcolorTwo: '#a3bcff',
-      bgGray: '#f5f5f5',
+  
+    extend: {
+      colors: {
+        bgcolor: "#060278",
+        textred: "#cf6a0c",
+        bghover: "#c7d9f0",
+        bgcolorTwo: "#a3bcff",
+        bgGray: "#f5f5f5",
+      },
     },
-    extend: {},
   },
   plugins: [],
-}
+};
